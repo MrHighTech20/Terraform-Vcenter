06a3f7a8a2af4104bd44140b3eb0ba55798e17e1
MrHighTech20 <marcoscantelli@hotmail.com>
2024-03-19 09:23:05 -0300

    criando main

---
+++ main.tf
@@ -0,0 +1,58 @@
+# main.tf
+
+# Configure you provider, in this case one server vcenter (vsphere)
+
+provider "vsphere" {
+  user           = "adminstrator@mvrc.local"
+  password       = "Mvrc#2022"
+  vsphere_server = "192.168.31.13"
+}
+
+# Set your datacenter created on vcenter
+
+data "vsphere_datacenter" "dc" {
+  name = "MVRC-DC"
+}
+
+# If you was one cluse, set here
+
+data "vsphere_cluster" "cluster" {
+  name          = "Xeon"
+  datacenter_id = data.vsphere_datacenter.dc.id
+}
+
+# Set parameters for your VM
+
+resource "vsphere_virtual_machine" "ubuntu_vm" {
+  name             = "UbuntuServer"
+  resource_pool_id = data.vsphere_cluster.cluster.resource_pool_id
+  datastore_id     = "ESX2"
+  folder           = "Ubuntu_Server"
+
+  num_cpus = 2
+  memory   = 4096
+
+  guest_id = "ubuntu64Guest"
+
+  network_interface {
+    network_id = "VM Network"
+  }
+
+  disk {
+    label = "disk0"
+    size  = 20
+  }
+
+  #cdrom {
+  #  datastore_iso_file = "seu_iso_path"
+  #}
+
+  clone {
+    template_uuid = "UbuntuServer"
+  }
+
+  # Adicione a chave pública à VM
+  provisioner "file" {
+    source      = "C:\\Users\\marco\\.ssh\\id_rsa"
+    destination = "/home/ubuntu/.ssh/authorized_keys"
+}
