88e87a8adc49e2690792b59ed1e6634576728e66
MrHighTech20 <marcoscantelli@hotmail.com>
2025-09-10 08:53:22 -0300

    modificando variaveis

--- variables.tf
+++ variables.tf
@@ -13,3 +13,59 @@ variable "vsphere_server" {
   description = "vSphere server address"
   type        = string
 }
+
+variable "datacenter_name" {
+  description = "Name of the vSphere datacenter"
+  type        = string
+}
+
+variable "datastore_name" {
+  description = "Name of the vSphere datastore"
+  type        = string
+}
+
+variable "network_name" {
+  description = "Name of the vSphere network"
+  type        = string
+}
+
+variable "vm_name" {
+  description = "Name of the virtual machine"
+  type        = string
+}
+
+variable "cpu_count" {
+  description = "Number of CPUs for the virtual machine"
+  type        = number
+}
+
+variable "memory_size" {
+  description = "Memory size (in MB) for the virtual machine"
+  type        = number
+}
+
+variable "guest_id" {
+  description = "Guest OS identifier for the virtual machine"
+  type        = string
+}
+
+variable "disk_size" {
+  description = "Disk size (in GB) for the virtual machine"
+  type        = number
+}
+
+variable "initial_username" {
+  description = "Initial username for the virtual machine"
+  type        = string
+}
+
+variable "initial_password" {
+  description = "Initial password for the virtual machine"
+  type        = string
+  sensitive   = true
+}
+
+variable "timezone" {
+  description = "Timezone for the virtual machine"
+  type        = string
+}
